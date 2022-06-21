94656c3875c70b7937d2d370102a4158b00a9a6d
Ansuman Mohanty <ansumanm20@gmail.com>
2022-06-21 20:05:09 +0530

    request response changes
    request response changes

--- DataTransformationLambda/HelloWorldFunction/src/main/java/datatransformation/PostHandler.java
+++ DataTransformationLambda/HelloWorldFunction/src/main/java/datatransformation/PostHandler.java
@@ -1,10 +1,12 @@
 package datatransformation;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * Handler for requests to Lambda function.
@@ -12,9 +14,23 @@ import java.util.Map;
 public class PostHandler implements RequestHandler<Map<String, String>, Map<String, String>> {
 
     public Map<String, String> handleRequest(final Map<String, String> input, final Context context) {
+
+        String firstName = input.get("firstName");
+        String lastname = input.get("lastName");
+        String email = input.get("email");
+        String password = input.get("password");
+        String repeatPassword = input.get("repeatPassword");
+
+        LambdaLogger logger = context.getLogger();
+
+        logger.log(firstName + "-" + lastname + "-" + email + "-" + password + "-" + repeatPassword);
+
         Map<String, String> response = new HashMap<>();
+        response.put("firstName",firstName);
+        response.put("lastName",lastname);
+        response.put("email",email);
+        response.put("id", UUID.randomUUID().toString());
 
         return response;
     }
-
 }
