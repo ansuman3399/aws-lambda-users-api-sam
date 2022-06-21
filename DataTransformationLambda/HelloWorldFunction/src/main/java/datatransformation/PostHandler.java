58c2c6837625eb6b0f118afe0ef380ee8c9e65dc
Ansuman Mohanty <ansumanm20@gmail.com>
2022-06-21 19:16:35 +0530

    renamed class
    renamed class

--- DataTransformationLambda/HelloWorldFunction/src/main/java/datatransformation/PostHandler.java
+++ DataTransformationLambda/HelloWorldFunction/src/main/java/datatransformation/PostHandler.java
@@ -14,7 +14,7 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 /**
  * Handler for requests to Lambda function.
  */
-public class App implements RequestHandler<Object, Object> {
+public class PostHandler implements RequestHandler<Object, Object> {
 
     public Object handleRequest(final Object input, final Context context) {
         Map<String, String> headers = new HashMap<>();
