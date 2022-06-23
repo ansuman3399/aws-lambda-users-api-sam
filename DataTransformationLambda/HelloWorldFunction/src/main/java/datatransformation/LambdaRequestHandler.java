9427dd05f88ab6417db27cabba893971aede7da0
Ansuman Mohanty <ansumanm20@gmail.com>
2022-06-23 19:33:39 +0530

    RequestStreamHandler interface
    RequestStreamHandler interface

--- DataTransformationLambda/HelloWorldFunction/src/main/java/datatransformation/LambdaRequestHandler.java
+++ DataTransformationLambda/HelloWorldFunction/src/main/java/datatransformation/LambdaRequestHandler.java
@@ -2,6 +2,10 @@ package datatransformation;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class LambdaRequestHandler implements RequestHandler<String, String> {
     public String handleRequest(String input, Context context) {
@@ -9,4 +13,12 @@ public class LambdaRequestHandler implements RequestHandler<String, String> {
         return "Hello World - " + input;
     }
 
+//    public class LambdaRequestStreamHandler
+//            implements RequestStreamHandler {
+//        public void handleRequest(InputStream inputStream,
+//                                  OutputStream outputStream, Context context) {
+//            String input = IOUtils.toString(inputStream, "UTF-8");
+//            outputStream.write(("Hello World - " + input).getBytes());
+//        }
+//    }
 }
