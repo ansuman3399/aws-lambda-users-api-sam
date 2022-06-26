082d4036eea8bce90407003182f0b09301c5b4ab
Ansuman Mohanty <ansumanm20@gmail.com>
2022-06-26 19:48:18 +0530

    aws lambda versioning
    aws lambda versioning

---
+++ LambdaVersionExample/src/main/java/com/appsdeveloperblog/aws/lambda/LambdaVersionExample.java
@@ -0,0 +1,25 @@
+package com.appsdeveloperblog.aws.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class LambdaVersionExample implements RequestHandler<APIGatewayProxyRequestEvent,
+        APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input,
+                                                      final Context context) {
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withBody("Code version '2'. Pred-production Deployment version: "
+                        + context.getFunctionVersion());
+
+        return response;
+
+    }
+
+}
