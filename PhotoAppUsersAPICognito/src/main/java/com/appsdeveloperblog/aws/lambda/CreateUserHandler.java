576232fe35d9263ffb82f45ff7799ca586941a73
Ansuman Mohanty <ansumanm20@gmail.com>
2022-06-29 19:53:41 +0530

    CreateUserHandler changes
    CreateUserHandler changes

--- PhotoAppUsersAPICognito/src/main/java/com/appsdeveloperblog/aws/lambda/CreateUserHandler.java
+++ PhotoAppUsersAPICognito/src/main/java/com/appsdeveloperblog/aws/lambda/CreateUserHandler.java
@@ -1,20 +1,32 @@
 package com.appsdeveloperblog.aws.lambda;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.appsdeveloperblog.aws.lambda.service.CognitoUserService;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Handler for requests to Lambda function.
  */
 public class CreateUserHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    private final CognitoUserService cognitoUserService;
+    private final String appClientId;
+    private final String appClientSecret;
+
+    public CreateUserHandler(CognitoUserService cognitoUserService, String appClientId, String appClientSecret) {
+        this.appClientId = System.getenv("MY_COGNITO_POOL_APP_CLIENT_ID");
+        this.appClientSecret = System.getenv("MY_COGNITO_POOL_APP_CLIENT_SECRET");
+        this.cognitoUserService = new CognitoUserService(System.getenv("AWS_REGION"));
+    }
+
 
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
 
@@ -32,10 +44,10 @@ public class CreateUserHandler implements RequestHandler<APIGatewayProxyRequestE
 
         JsonObject userDetails = JsonParser.parseString(requestBody).getAsJsonObject();
 
+        JsonObject createUserResponse = cognitoUserService.createUser(userDetails, appClientId, appClientSecret);
         return response
                 .withStatusCode(200)
                 .withBody("{}");
-
     }
 
 }
