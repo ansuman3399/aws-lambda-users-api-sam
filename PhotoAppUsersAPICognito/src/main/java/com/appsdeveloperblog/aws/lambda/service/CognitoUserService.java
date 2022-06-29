7163a5c131d27f6df0c1b7e7d114ef94beff0edc
Ansuman Mohanty <ansumanm20@gmail.com>
2022-06-29 19:20:05 +0530

    SignUpRequest
    SignUpRequest

--- PhotoAppUsersAPICognito/src/main/java/com/appsdeveloperblog/aws/lambda/service/CognitoUserService.java
+++ PhotoAppUsersAPICognito/src/main/java/com/appsdeveloperblog/aws/lambda/service/CognitoUserService.java
@@ -1,11 +1,19 @@
 package com.appsdeveloperblog.aws.lambda.service;
 
 import com.google.gson.JsonObject;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.SignUpRequest;
 
 public class CognitoUserService {
 
-    public JsonObject createUser(JsonObject user){
+    public JsonObject createUser(JsonObject user) {
         JsonObject storedUser = new JsonObject();
+
+        String userName = user.get("email").getAsString();
+        String password = user.get("password").getAsString();
+        SignUpRequest signUpRequest = SignUpRequest.builder()
+                .username(userName)
+                .password(password)
+                .build();
         return storedUser;
     }
 }
