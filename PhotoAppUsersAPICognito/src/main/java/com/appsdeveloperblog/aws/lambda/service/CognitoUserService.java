4fa68b2e36b878895e5ce116f8779bbd7b8fc8fc
Ansuman Mohanty <ansumanm20@gmail.com>
2022-06-29 19:40:03 +0530

    CognitoIdentityProviderClient
    CognitoIdentityProviderClient

--- PhotoAppUsersAPICognito/src/main/java/com/appsdeveloperblog/aws/lambda/service/CognitoUserService.java
+++ PhotoAppUsersAPICognito/src/main/java/com/appsdeveloperblog/aws/lambda/service/CognitoUserService.java
@@ -1,16 +1,27 @@
 package com.appsdeveloperblog.aws.lambda.service;
 
 import com.google.gson.JsonObject;
+import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
 import software.amazon.awssdk.services.cognitoidentityprovider.model.AttributeType;
 import software.amazon.awssdk.services.cognitoidentityprovider.model.SignUpRequest;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.List;
 import java.util.UUID;
 
 public class CognitoUserService {
 
-    public JsonObject createUser(JsonObject user , String appClientId) {
+    private final CognitoIdentityProviderClient client;
+
+    public CognitoUserService(CognitoIdentityProviderClient client) {
+        this.client = client;
+    }
+
+    public JsonObject createUser(JsonObject user, String appClientId, String appClientSecret) {
 
         JsonObject storedUser = new JsonObject();
 
@@ -37,14 +48,34 @@ public class CognitoUserService {
 
         List<AttributeType> attributes = Arrays.asList(emailAttribute, nameAttribute, attributeUserId);
 
+        String secretHash = calculateSecretHash(appClientId, appClientSecret, email);
+
         SignUpRequest signUpRequest = SignUpRequest.builder()
                 .username(email)
                 .password(password)
                 .userAttributes(attributes)
                 .clientId(appClientId)
-                .secretHash("")
+                .secretHash(secretHash)
                 .build();
 
         return storedUser;
     }
+
+    public static String calculateSecretHash(String userPoolClientId, String userPoolClientSecret, String userName) {
+        final String HMAC_SHA256_ALGORITHM = "HmacSHA256";
+
+        SecretKeySpec signingKey = new SecretKeySpec(
+                userPoolClientSecret.getBytes(StandardCharsets.UTF_8),
+                HMAC_SHA256_ALGORITHM);
+        try {
+            Mac mac = Mac.getInstance(HMAC_SHA256_ALGORITHM);
+            mac.init(signingKey);
+            mac.update(userName.getBytes(StandardCharsets.UTF_8));
+            byte[] rawHmac = mac.doFinal(userPoolClientId.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(rawHmac);
+        } catch (Exception e) {
+            throw new RuntimeException("Error while calculating ");
+        }
+    }
+
 }
