60bbc3c68ecfc3a8a3e2210f7fbacd5eea2d48b2
Ansuman Mohanty <ansumanm20@gmail.com>
2022-06-29 19:28:57 +0530

    more sign up request changes
    more sign up request changes

--- PhotoAppUsersAPICognito/src/main/java/com/appsdeveloperblog/aws/lambda/service/CognitoUserService.java
+++ PhotoAppUsersAPICognito/src/main/java/com/appsdeveloperblog/aws/lambda/service/CognitoUserService.java
@@ -1,19 +1,50 @@
 package com.appsdeveloperblog.aws.lambda.service;
 
 import com.google.gson.JsonObject;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.AttributeType;
 import software.amazon.awssdk.services.cognitoidentityprovider.model.SignUpRequest;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 public class CognitoUserService {
 
-    public JsonObject createUser(JsonObject user) {
+    public JsonObject createUser(JsonObject user , String appClientId) {
+
         JsonObject storedUser = new JsonObject();
 
-        String userName = user.get("email").getAsString();
+        String email = user.get("email").getAsString();
         String password = user.get("password").getAsString();
-        SignUpRequest signUpRequest = SignUpRequest.builder()
-                .username(userName)
-                .password(password)
+        String userId = UUID.randomUUID().toString();
+        String firstName = user.get("firstName").getAsString();
+        String lastName = user.get("lastName").getAsString();
+
+        AttributeType emailAttribute = AttributeType.builder()
+                .name("email")
+                .value(email)
                 .build();
+
+        AttributeType nameAttribute = AttributeType.builder()
+                .name("name")
+                .value(firstName + " " + lastName)
+                .build();
+
+        AttributeType attributeUserId = AttributeType.builder()
+                .name("custom:userId")
+                .value(userId)
+                .build();
+
+        List<AttributeType> attributes = Arrays.asList(emailAttribute, nameAttribute, attributeUserId);
+
+        SignUpRequest signUpRequest = SignUpRequest.builder()
+                .username(email)
+                .password(password)
+                .userAttributes(attributes)
+                .clientId(appClientId)
+                .secretHash("")
+                .build();
+
         return storedUser;
     }
 }
